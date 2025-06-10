a589ad1d248caf410cc6ce0aebe5a3d6bf58e87e
Dheeraj Abhishek <dheerajabhishek111@gmail.com>
2025-06-10 21:52:03 +0530

    changed from bycrypt to bycryptjs

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path")
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const session = require("express-session");
 
 
