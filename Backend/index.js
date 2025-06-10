76f3a951cb4ad31401324c3588c4d14cbee54db4
Dheeraj Abhishek <dheerajabhishek111@gmail.com>
2025-06-11 01:55:02 +0530

    final 3

--- Backend/index.js
+++ Backend/index.js
@@ -48,10 +48,11 @@ app.use(session({
     },
 }));
 
-// ✅ This line needs path
-// app.use(express.static(path.join(__dirname, "FrontEnd")));
-app.use(express.static('public'));
-app.use(express.json());
+app.use(express.static(path.join(__dirname, "frontend-build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend-build/index.html"));
+});
 
 
 
